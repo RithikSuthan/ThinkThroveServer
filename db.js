d202cff6e1169259e08364e23309d913b24cdea7
Swetha R <swetharamasamy378@gmail.com>
2024-08-07 18:38:34 +0530

    db

--- db.js
+++ db.js
@@ -12,6 +12,7 @@ const loginValidation = require("./MiddleWare/SignIn.js");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
